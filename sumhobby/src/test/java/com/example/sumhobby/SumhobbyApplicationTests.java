3f145dd9a5f830cef4af015bb6b1de73d84b083f
yeonsu10 <jenix1@naver.com>
2023-06-20 10:31:33 +0900

    feat: 초기 설정

---
+++ sumhobby/src/test/java/com/example/sumhobby/SumhobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumhobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumhobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
