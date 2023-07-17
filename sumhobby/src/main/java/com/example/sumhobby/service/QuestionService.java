83eb122980d73db7c91cdbe007c0989815ef6db7
monaYR <rufinayk@naver.com>
2023-07-17 18:26:47 +0900

    fix: QuestionService merge 오류 수정

--- sumhobby/src/main/java/com/example/sumhobby/service/QuestionService.java
+++ sumhobby/src/main/java/com/example/sumhobby/service/QuestionService.java
@@ -15,34 +15,32 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class QuestionService {
-	
+
 	@Autowired
 	private QuestionRepository quesRepository;
 
 	public QuestionEntity selectOne(final int quesNum) {
 		return quesRepository.findById(quesNum).get();
 	}
+
 	public QuestionEntity create(QuestionEntity quesEntity) {
-		if(quesEntity == null) {
+		if (quesEntity == null) {
 			log.warn("Entity cannot be null");
 			throw new RuntimeException("Entity cannot be null");
 		}
 		return quesRepository.save(quesEntity);
 	}
-	
-	public List<QuestionEntity> selectByLectureRef(LectureEntity lecEntity){
+
+	public List<QuestionEntity> selectByLectureRef(LectureEntity lecEntity) {
 		return quesRepository.findByLecRef(lecEntity);
 	}
-	
+
 	public List<QuestionEntity> matchUserTk(Integer lecNum, String userTk) {
-		  return quesRepository.matchUserTk(lecNum, userTk);
-		}
-	
+		return quesRepository.matchUserTk(lecNum, userTk);
+	}
+
 	public void saveQuestion(QuestionEntity question) {
-		  quesRepository.save(question);
-		}
-	
-	public QuestionEntity getQuestionById(Integer lecNum, Integer quesNum) {
-		  return quesRepository.findByLecNumAndQuesNum(lecNum, quesNum);
-		}
+		quesRepository.save(question);
+	}
+
 }
