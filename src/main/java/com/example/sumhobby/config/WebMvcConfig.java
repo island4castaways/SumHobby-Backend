ef25f14d11687f990d851378ac89ba2ca136bcaf
monaYR <rufinayk@naver.com>
2023-07-21 11:18:05 +0900

    feat: domain changed

--- src/main/java/com/example/sumhobby/config/WebMvcConfig.java
+++ src/main/java/com/example/sumhobby/config/WebMvcConfig.java
@@ -12,7 +12,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 		.allowedOrigins("http://localhost:3000",
-				"http://sumhobby-frontend2-env.eba-awrmpsam.ap-northeast-2.elasticbeanstalk.com/")
+				"http://sumhobby-frontend2-env.eba-awrmpsam.ap-northeast-2.elasticbeanstalk.com",
+				"https://app.sumhobby.com")
 		.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 		.allowedHeaders("*")
 		.allowCredentials(true)
